99c6cf9d72db10a664c985e2562999a396407eee
ignace-bossuyt <ignace.bossuyt@student.kuleuven.be>
2019-09-04 22:55:10 +0200

    changed structure of main and added preparation for discussion

--- introductory_exercises/limb_darkening/test_file.m
+++ introductory_exercises/limb_darkening/test_file.m
@@ -6,6 +6,12 @@ maximum_optical_depth = 10
 
 Limb_Darkening(number_of_channels,number_of_photons,maximum_optical_depth)
 
+save = 1
+if save == 1
+    tit = ['number_channels',num2str(number_of_channels),'number_photons',num2str(number_of_photons),'max_opt_depth',num2str(maximum_optical_depth),'.png']
+    saveas(gcf,tit)
+end
+
 %%
 clc, clear, close all
 
